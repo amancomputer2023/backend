1013b1536d045b6d438c773c87a81471cc75a179
Aman Computer <amancomputer005@gmail.com>
2025-03-04 16:54:44 +0530

    .

--- models/NewsletterModel.js
+++ models/NewsletterModel.js
@@ -2,12 +2,12 @@ const { getDB } = require("../config/db");
 
 const findNewsLetterSubscriber = async(email) => {
     const db = getDB();
-    return await db.collection("Newsletter").findone({email});
+    return await db.collection("newsletter").findone({email});
 }
 
 const insertNewsLetterSubscriber = async () => {
     const db = getDB();
-    return await db.collection("Newsletter").insertOne(service);
+    return await db.collection("newsletter").insertOne(service);
 };
 
 module.exports = { insertNewsLetterSubscriber, findNewsLetterSubscriber };
